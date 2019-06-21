e4069db366412884b94f00be00bf7e0ad496ef9a
Kushani <wakhansika1994@gmail.com>
2019-06-21 13:28:18 +0530

    initial commit

---
+++ POS-WEB-1/src/main/java/lk/ijse/dep/pos/spring/last/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep.pos.spring.last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class,args);
+    }
+
+}
